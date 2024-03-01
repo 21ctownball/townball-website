90c2a7dff6b10b1f536d24d516b07ef2dc63cb72
Rylan Schubkegel <rylanschubkegel@gmail.com>
2024-02-29 20:52:17 -0800

    Better rules page, closes #23

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -17,3 +17,8 @@ export function getFormattedDatetime(datetime: string) {
 export function isHrefExternal(href: string) {
   return /^https?:\/\//.test(href);
 }
+
+export function scrollToElement(el: HTMLElement, { behavior = 'smooth', offset = 0 }: { behavior?: ScrollBehavior; offset?: number } = {}) {
+  const top = el.getBoundingClientRect().top + window.scrollY + offset;
+  window.scrollTo({ top, behavior });
+}
