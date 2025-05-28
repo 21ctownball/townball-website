c690f3f524b470a09d00a6a9fe88dcc5dab89ef9
Rylan Schubkegel <rylanschubkegel@gmail.com>
2025-05-28 15:23:25 -0700

    Address empty string number conversion

--- src/scripts/schemas.ts
+++ src/scripts/schemas.ts
@@ -12,7 +12,7 @@ export const abbreviationSchema = z.array(
 
 export const leagueSchema = z.array(
   z.object({
-    id: z.coerce.number().describe('GUID of league'),
+    id: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of league'),
     acronym: z.string().describe('Acronym of league'),
     name: z.string().describe('Full name of league'),
   }),
@@ -20,13 +20,13 @@ export const leagueSchema = z.array(
 
 export const gameSchema = z.array(
   z.object({
-    at_bat: z.coerce.number().describe('The at-bat number (sequential, starting at 0)'),
+    at_bat: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('The at-bat number (sequential, starting at 0)'),
     batting_team: z.string().regex(TEAM_ID_REGEX, 'Team composite key must be in format <team_id>-YYYY').describe('Composite key of batting team'),
-    batter: z.coerce.number().describe('GUID of batter'),
+    batter: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of batter'),
     pitching_team: z.string().regex(TEAM_ID_REGEX, 'Team composite key must be in format <team_id>-YYYY').describe('Composite key of pitching team'),
-    pitcher: z.coerce.number().describe('GUID of pitcher'),
-    attempt: z.coerce.number().nullable(),
-    rbi: z.coerce.number().describe('How many runs did the hitter bat in?'),
+    pitcher: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of pitcher'),
+    attempt: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).nullable(),
+    rbi: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('How many runs did the hitter bat in?'),
     single: z.coerce.boolean().describe('Was this a single?'),
     double: z.coerce.boolean().describe('Was this a double?'),
     triple: z.coerce.boolean().describe('Was this a triple?'),
@@ -47,20 +47,19 @@ export const gameSchema = z.array(
     flyout: z.coerce.boolean().describe('Did the batter fly out?'),
     tag: z.coerce.boolean().describe('Was the batter tagged out?'),
     peg: z.coerce.boolean().describe("Was the batter pegged out?"),
-    //also wondering if ".nullable" means that it will detect " " seperately from "0".  if it does not, or if the existence of an ID#0 would cause issues, we can assign the player with ID#0 to another number
-    fielder: z.string().transform(s => !s.length ? null : Number(s)).describe('GUID of fielder'),
-    assist: z.string().transform(s => !s.length ? null : Number(s)).describe('GUID of player who made the assist'),
-    assist_1: z.string().transform(s => !s.length ? null : Number(s)),
-    assist_2: z.string().transform(s => !s.length ? null : Number(s)),
-    assist_3: z.string().transform(s => !s.length ? null : Number(s)),
-    assist_4: z.string().transform(s => !s.length ? null : Number(s)),
-    assist_5: z.string().transform(s => !s.length ? null : Number(s)),
-    assist_6: z.string().transform(s => !s.length ? null : Number(s)),
-    error: z.string().transform(s => !s.length ? null : Number(s)).describe('GUID of player who made the error'),
-    error_1: z.string().transform(s => !s.length ? null : Number(s)),
-    error_2: z.string().transform(s => !s.length ? null : Number(s)),
-    error_3: z.string().transform(s => !s.length ? null : Number(s)),
-    error_4: z.string().transform(s => !s.length ? null : Number(s)),
+    fielder: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of fielder'),
+    assist: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of player who made the assist'),
+    assist_1: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    assist_2: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    assist_3: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    assist_4: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    assist_5: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    assist_6: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    error: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of player who made the error'),
+    error_1: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    error_2: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    error_3: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
+    error_4: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)),
   }).describe('A single at bat'),
 );
 
@@ -74,7 +73,7 @@ export const gameFileSchema = z.array(
 
 export const playerSchema = z.array(
   z.object({
-    id: z.coerce.number().describe('GUID'),
+    id: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID'),
     last_name: z.string().describe('Last name of player'),
     first_name: z.string().describe('First name of player'),
     debut: z.coerce.date().describe('Date player debuted'),
@@ -83,8 +82,8 @@ export const playerSchema = z.array(
 
 export const teamSchema = z.array(
   z.object({
-    league_id: z.coerce.number().describe('GUID of league'),
-    club_id: z.coerce.number().describe('GUID of club'),
+    league_id: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of league'),
+    club_id: z.string().transform(s => !s.length ? null : Number(s.trim() || NaN)).describe('GUID of club'),
     acronym: z.string().describe('Three letter acronym of team'),
     name: z.string().describe('Name of team'),
     season: z.string().describe('Year this team played (season) in format YYYY'),
